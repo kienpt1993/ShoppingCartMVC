a4898f30262491fd6c35a289d05016bb8015b1df
kienpt1993 <luckyboyuit06@gmail.com>
2015-03-23 10:39:00 +0700

    f
    f

---
+++ ShoppingCartMvc/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCartMvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+        public ActionResult Index()
+        {
+            // ADO.NET
+            return View();
+        }
+	}
+}
+// Them xoa sua danh muc, them xoa sua san pham ... Backend
